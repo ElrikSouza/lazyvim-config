d98c344383aa746d8bdd7e9837c30a63c4deac77
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2023-12-12 13:36:31 -0400

    add config

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    keys = {
+      { "<leader>bj", "<cmd>BufferLinePick<cr>", desc = "Jump" },
+    },
+  },
+  {
+    "echasnovski/mini.nvim",
+    keys = {
+      { "<leader>d", "<cmd><cr>" },
+    },
+  },
+}
